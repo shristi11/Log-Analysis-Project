e848797757bd568fa1c76cc92957bfaf25981dfb
shristi11 <shristi.ui@gmail.com>
2017-11-15 15:48:19 -0500

    Update newslog.py

--- newslog.py
+++ newslog.py
@@ -5,15 +5,15 @@ import psycopg2
 DBNAME = "news"
 
 
-def  get_conn(query):
-     ''' Connect to database '''
-     db = psycopg2.connect(database=DBNAME)
-     c = db.cursor()
-     c.execute(query)
-     return c.fetchall()
-     db.close()
-        
-
+def get_conn(query):
+    ''' Connect to database '''
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute(query)
+    return c.fetchall()
+    db.close()
+     
+     
 query1 = """
       SELECT articles.title, Count(*) As views
       FROM articles
@@ -54,27 +54,27 @@ query3 = """
       WHERE (ROUND(((errors.error_requests*1.0) / total.requests), 3) > 0.01)
       ORDER BY percent DESC;
 """
-   
-    
+
+
 def first_query(query):
     results = get_conn(query)
     print('\n1. The most popular three articles of all time are:\n')
     for result in results:
-        print ('  ' + str(result[0]) + ' - ' + str(result[1]) + ' views')
+        print('  ' + str(result[0]) + ' - ' + str(result[1]) + ' views')
         
 
 def second_query(query):
     results = get_conn(query)
     print('\n2. The most popular article authors of all time are:\n')
     for result in results:
-        print ('  ' + str(result[0]) + ' - ' + str(result[1]) + ' views')
+        print('  ' + str(result[0]) + ' - ' + str(result[1]) + ' views')
 
         
 def third_query(query):
     results = get_conn(query)
     print('\n3. The day on which more than 1 percent of requests lead to errors:\n')
     for result in results:
-        print ('  ' + str(result[0]) + ' - ' + str(result[1]) + ' %')
+        print('  ' + str(result[0]) + ' - ' + str(result[1]) + ' %')
 
 
 first_query(query1)
