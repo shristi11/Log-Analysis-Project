3a98cd86bba7be6956226eb581cd9d00115a01bd
shristi11 <shristi.ui@gmail.com>
2017-11-16 11:59:49 -0500

    Update newslog.py

--- newslog.py
+++ newslog.py
@@ -1,8 +1,8 @@
-#! /usr/bin/env
+#! /usr/bin/env python
 
 import psycopg2
 
-DBNAME = "news"
+DBNAME="news"
 
 
 def get_conn(query):
@@ -10,15 +10,16 @@ def get_conn(query):
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
     c.execute(query)
-    return c.fetchall()
+    res = c.fetchall()
     db.close()
-     
-     
+    return res
+
+
 query1 = """
       SELECT articles.title, Count(*) As views
       FROM articles
       JOIN log
-      ON log.path LIKE concat('/article/%', articles.slug)
+      ON log.path = concat('/article/', articles.slug)
       GROUP BY articles.title
       ORDER by views DESC
       LIMIT 3;
@@ -30,23 +31,22 @@ query2 = """
       JOIN articles
       ON authors.id = articles.author
       JOIN log
-      ON log.path LIKE concat('/article/%', articles.slug)
+      ON log.path = concat('/article/', articles.slug)
       GROUP BY authors.name
-      ORDER by views DESC
-      LIMIT 3;
+      ORDER by views DESCa;
 """
 
 query3 = """
       SELECT total.day,
           ROUND(((errors.error_requests*1.0) / total.requests), 3) AS percent
       FROM (
-          SELECT date_trunc('day', time) "day", count(*) AS error_requests
+          SELECT time::date as day, count(*) AS error_requests
           FROM log
           WHERE status LIKE '404%'
           GROUP BY day
         ) AS errors
       JOIN (
-          SELECT date_trunc('day', time) "day", count(*) AS requests
+          SELECT time::date as day, count(*) AS requests
           FROM log
           GROUP BY day
           ) AS total
@@ -54,29 +54,34 @@ query3 = """
       WHERE (ROUND(((errors.error_requests*1.0) / total.requests), 3) > 0.01)
       ORDER BY percent DESC;
 """
-
+   
 
 def first_query(query):
     results = get_conn(query)
     print('\n1. The most popular three articles of all time are:\n')
-    for result in results:
-        print('  ' + str(result[0]) + ' - ' + str(result[1]) + ' views')
+    for title, views in results:
+        print(' "{}" -- {} views'.format(title, views))
         
 
 def second_query(query):
     results = get_conn(query)
     print('\n2. The most popular article authors of all time are:\n')
-    for result in results:
-        print('  ' + str(result[0]) + ' - ' + str(result[1]) + ' views')
+    for name, views in results:
+        print(' "{}" -- {} views'.format(name, views))
 
-        
+
 def third_query(query):
     results = get_conn(query)
     print('\n3. The day on which more than 1 percent of requests lead to errors:\n')
     for result in results:
-        print('  ' + str(result[0]) + ' - ' + str(result[1]) + ' %')
+        print('  {0:%B %d, %Y} - {1:%} errors'.format(result[0], result[1]))
 
 
-first_query(query1)
-second_query(query2)
-third_query(query3)
+def main():
+    first_query(query1)
+    second_query(query2)
+    third_query(query3)
+
+
+if __name__ == '__main__':
+  main()
